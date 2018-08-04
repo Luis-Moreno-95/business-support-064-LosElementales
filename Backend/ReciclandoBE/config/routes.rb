bfd2c9a203dd45264660bb7e1d43dc768a75899c
Julieth Cobos <jcobosg@unbosque.edu.co>
2018-08-03 20:41:41 -0500

    Api roles

--- Backend/ReciclandoBE/config/routes.rb
+++ Backend/ReciclandoBE/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :roles
+    root 'roles#index'
+
+  namespace :api, defaults: {format: 'json'} do
+    resources :roles, only: [:index, :create, :destroy, :update, :show]
+  end
 end
