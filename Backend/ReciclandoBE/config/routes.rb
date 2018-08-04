e0f8fe2acc3af6eeb31a9cadea472b475175d554
Wilson Arguello <wfarguelloc@gmail.com>
2018-08-03 21:01:56 -0500

    Api tabla Clasification

--- Backend/ReciclandoBE/config/routes.rb
+++ Backend/ReciclandoBE/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :clasifications
+  root 'clasifications#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: 'json'} do
+    resources :clasifications, only: [:index, :create, :destroy, :update, :show]
+  end
 end
