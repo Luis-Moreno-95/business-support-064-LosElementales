2d7db19564d5a64800ca45704e4006eb6c43aacc
unknown <wfarguelloc@gmail.com>
2018-11-07 20:29:14 -0500

    carpeta Assets Juego Unity

---
+++ Juego/Assets/Scipts/CamaraSigue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraSigue : MonoBehaviour {
+
+	public Transform objetivo;
+	public float suavizado = 5f;
+	Vector3 desface;
+
+	// Use this for initialization
+	void Start () {
+		 desface = transform.position - objetivo.position;
+
+	}
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	void FixedUpdate ()	{
+			Vector3 posicionObjetivo = objetivo.position + desface;
+			transform.position = Vector3.Lerp(transform.position, posicionObjetivo, suavizado*Time.deltaTime);
+	}
+
+}
